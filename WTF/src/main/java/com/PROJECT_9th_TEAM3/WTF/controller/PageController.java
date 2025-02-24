74c8e388a7f4a169f5caf32aaa5a14fee8b1a7ed
GAN <greer_00@naver.com>
2025-02-24 15:02:11 +0900

    페이지 연결 수정

--- WTF/src/main/java/com/PROJECT_9th_TEAM3/WTF/controller/PageController.java
+++ WTF/src/main/java/com/PROJECT_9th_TEAM3/WTF/controller/PageController.java
@@ -8,12 +8,18 @@ public class PageController {
     // 기본 페이지 요청
     @GetMapping("/")
     public String index() {
-        return "login";
+        return "index";
     }
 
     // 회원가입 페이지 요청
     @GetMapping("signup")
     public String signup() {
         return "signup";
+    }
+
+    // 로그인 페이지 요청
+    @GetMapping("login")
+    public String login() {
+        return "login";
     }
 }
